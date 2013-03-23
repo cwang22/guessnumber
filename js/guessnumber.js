4e19b9e3c448d811e37672b2765b89508e2fff13
C.Wang <wcg0304@sina.com>
2013-03-23 19:05:40 +1100

    visual improvement

--- js/guessnumber.js
+++ js/guessnumber.js
@@ -2,21 +2,13 @@ var numberlist = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9);
 var setnum = new Array();
 var times = 0;
 
-function guess() {
+function guessNumber() {
 	times++;
 	var attempt = new Array($("#guess0").val(), $("#guess1").val(), $("#guess2").val(), $("#guess3").val());
 	var a = 0;
 	var b = 0;
-	/*
-	 var attempt0 = $("#guess0").val();
-	 var attempt1 = $("#guess1").val();
-	 var attempt2 = $("#guess2").val();
-	 var attempt3 = $("#guess3").val();*/
-
 	for (var i = 0; i < 4; i++) {
 		for (var j = 0; j < 4; j++) {
-			//alert(attempt[j]);
-			//alert(setnum[i]);
 			if (attempt[j] == setnum[i]) {
 				if (j == i) {
 					a = a + 1;
@@ -31,10 +23,8 @@ function guess() {
 	if (a == 4) {
 		alert("yeah!");
 	} else {
-		$('#result').append('<div class="row"><div class="span4">' + times + '</div><div class="span4">' + attempt + '</div><div class="span4">' + a +"A" + b +"B</div></div>");
+		$('#result').append('<tr><td>' + times + '</td><td>' + attempt + '</td><td>' + a + "A" + b + "B</td></tr>");
 	}
-
-	//alert("hi");
 }
 
 
@@ -45,10 +35,5 @@ $(document).ready(function() {
 		setnum[i] = numberlist[r];
 		numberlist.splice(r, 1);
 	}
-
-	/*
-	 for(var i=0;i<setnum.length;i++){
-	 alert(setnum[i]);
-	 }*/
 
 });
